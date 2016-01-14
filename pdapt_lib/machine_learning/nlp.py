0b7e6116362b89cb8028940640eaa8c81cca5801
Matthew K. MacLeod <matthewmacleod28@gmail.com>
2016-01-14 12:49:57 -0700

    change n_gram and skip_gram to take input string

--- pdapt_lib/machine_learning/nlp.py
+++ pdapt_lib/machine_learning/nlp.py
@@ -148,11 +148,12 @@ def tokenize(s,n=1):
     s = lowercase(s)
     s = remove_punctuation(s)
     tokens = {}
-    for t in s.split(" "):
-        if t in tokens:
-            tokens[t] += 1
+    ngrams =  n_gram(n,s)
+    for w in ngrams:
+        if w in tokens:
+            tokens[w] += 1
         else:
-            tokens[t] = 1
+            tokens[w] = 1
     return tokens
 
 
@@ -162,34 +163,38 @@ def tokenize(s,n=1):
 
 def n_gram(n, words):
     """ n gram model
-    input: list of words
+    input: string
     output: list of n-grams from text
     NB to join sublists, [" ".join(i) for i in ngrams]
-    >>> n_gram(2, "the rain in Spain falls mainly on the plain".split(" "))
-    [['the', 'rain'], ['rain', 'in'], ['in', 'Spain'], ['Spain', 'falls'], ['falls', 'mainly'], ['mainly', 'on'], ['on', 'the'], ['the', 'plain']]
-    >>> n_gram(3, "the rain in Spain falls mainly on the plain".split(" "))
-    [['the', 'rain', 'in'], ['rain', 'in', 'Spain'], ['in', 'Spain', 'falls'], ['Spain', 'falls', 'mainly'], ['falls', 'mainly', 'on'], ['mainly', 'on', 'the'], ['on', 'the', 'plain']]
+    >>> n_gram(2, "the rain in Spain falls mainly on the plain")
+    ['the rain', 'rain in', 'in Spain', 'Spain falls', 'falls mainly', 'mainly on', 'on the', 'the plain']
+    >>> n_gram(3, "the rain in Spain falls mainly on the plain")
+    ['the rain in', 'rain in Spain', 'in Spain falls', 'Spain falls mainly', 'falls mainly on', 'mainly on the', 'on the plain']
+    >>> n_gram(1, "the rain in Spain falls mainly on the plain")
+    ['the', 'rain', 'in', 'Spain', 'falls', 'mainly', 'on', 'the', 'plain']
     """
+    words = words.split(" ")
     ngrams = [words[i:i+n] for i in range(len(words)-(n-1))]
-    return ngrams
+    return [" ".join(i) for i in ngrams]
 
 
 def skip_gram(k, n, words):
     """ skip grams
-    input: k (skip), n (as in n-gram), list of words
+    input: k (skip), n (as in n-gram), string words
     output: list of skip grams
     NB this is a bit trickier to do in one line...
-    >>> skip_gram(1, 2, "the rain in Spain falls mainly on the plain".split(" "))
-    [['the', 'in'], ['rain', 'Spain'], ['in', 'falls'], ['Spain', 'mainly'], ['falls', 'on'], ['mainly', 'the'], ['on', 'plain']]
-    >>> skip_gram(1, 3, "the rain in Spain falls mainly on the plain".split(" "))
-    [['the', 'in', 'falls'], ['rain', 'Spain', 'mainly'], ['in', 'falls', 'on'], ['Spain', 'mainly', 'the'], ['falls', 'on', 'plain']]
-    >>> skip_gram(2, 2, "the rain in Spain falls mainly on the plain".split(" "))
-    [['the', 'Spain'], ['rain', 'falls'], ['in', 'mainly'], ['Spain', 'on'], ['falls', 'the'], ['mainly', 'plain']]
-    >>> skip_gram(2, 3, "the rain in Spain falls mainly on the plain".split(" "))
-    [['the', 'Spain', 'on'], ['rain', 'falls', 'the'], ['in', 'mainly', 'plain']]
+    >>> skip_gram(1, 2, "the rain in Spain falls mainly on the plain")
+    ['the in', 'rain Spain', 'in falls', 'Spain mainly', 'falls on', 'mainly the', 'on plain']
+    >>> skip_gram(1, 3, "the rain in Spain falls mainly on the plain")
+    ['the in falls', 'rain Spain mainly', 'in falls on', 'Spain mainly the', 'falls on plain']
+    >>> skip_gram(2, 2, "the rain in Spain falls mainly on the plain")
+    ['the Spain', 'rain falls', 'in mainly', 'Spain on', 'falls the', 'mainly plain']
+    >>> skip_gram(2, 3, "the rain in Spain falls mainly on the plain")
+    ['the Spain on', 'rain falls the', 'in mainly plain']
     """
+    words = words.split(" ")
     sgrams = [list([words[i]]) + [words[i+k+j] for j in range(1,n+k,k+1)] for i in range(len(words)-((1+k)*(n-1)))]
-    return sgrams
+    return [" ".join(i) for i in sgrams]
 
 
 
