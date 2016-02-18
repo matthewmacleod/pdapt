67420d83b2781c21c89ee43719ab653936cda1eb
Matthew K. MacLeod <matthewmacleod28@gmail.com>
2016-02-18 14:35:52 -0700

    start some classification

---
+++ pdapt_lib/machine_learning/classification.py
@@ -0,0 +1,18 @@
+#  classification.py
+#
+#  Author: Matthew K. MacLeod
+#
+#  For license information see license.txt
+
+""" classifiction module
+
+
+"""
+
+import math, random
+from collections import defaultdict
+from pdapt_lib.machine_learning.maths import sum_of_squares, dot
+import numpy as np
+from math import sqrt
+
+
